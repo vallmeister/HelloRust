78fab277cdcfb9ee11b50685ddd63caf1aaeacc2
Christian <christian.valenti97@gmail.com>
2022-07-12 17:22:34 +0200

    Learned about basic data types :dango: :clock530:

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
+use std::mem;
+
 fn main() {
-    println!("Hello, world!");
+    hello_world();
+
+    // basic data types
+    let a: u8 = 123; // u = unsigned, 8 bits, 0...255
+    println!("a = {}", a); // ! means makro
+    // all variables are immutable by default
+
+    // u = unsigned
+    // i = signed
+    let mut b: i8 = 0; // mutable 8-bit integer
+    println!("b = {} before", b);
+    b = 42;
+    println!("b = {} after", b);
+
+    // Rust can do type inference
+
+    let mut c = 123456789; // i32
+    println!("c = {}, takes up {} bytes", c, mem::size_of_val(&c));
+    c = -1;
+    println!("c = {}", c);
+
+    // usize isize: bitness of computer
+    let z: isize = 123;
+    let size_of_z = mem::size_of_val(&z);
+    println!("z = {}, takes up {} bytes, {}-bit OS", z, size_of_z, size_of_z * 8);
+}
+
+// #1 Hello world
+fn hello_world() {
+    println!("Hello world");
 }
